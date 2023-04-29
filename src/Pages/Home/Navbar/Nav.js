9b45d28c50ff0ab712929addd877a2f1490dc76b
Iftekharshovon01 <iftekharshovonseu17@gmail.com>
2023-04-29 16:08:00 +0600

    menue bar

--- src/Pages/Home/Navbar/Nav.js
+++ src/Pages/Home/Navbar/Nav.js
@@ -1,96 +1,49 @@
-import React, { useState } from 'react';
-import logo from '../../../Assets/img/Logo/logo.png';
 import { Col, Container, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBagShopping, faBars, faCartShopping, faChevronDown, faHeadset, faHeart, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import { faBagShopping, faChevronDown, } from '@fortawesome/free-solid-svg-icons';
+import Searchbar from './Searchbar';
 
 const Nav = () => {
-    const [searchQuery, setSearchQuery] = useState('');
 
-    const handleSearch = (event) => {
-        setSearchQuery(event.target.value);
-    }
-    const redirectCall = (phoneNumber) => {
-        window.location.href = `tel:${phoneNumber}`;
-    };
 
     return (
         <>
+            <Searchbar></Searchbar>
             <Container>
-                <Row className='align-items-center justify-content-between py-4'>
-                    <Col md={2}>
-                        <div className="head-logo">
-                            <img className='w-100' src={logo} alt="" />
-                        </div>
-                    </Col>
-                    <Col md={5}>
-                        <div className="head-search">
-                            <div className="search-container">
-                                <input className='search-input'
-                                    type="text"
-                                    placeholder="Search Here"
-                                    value={searchQuery}
-                                    onChange={handleSearch}
-                                />
-                            </div>
-                            <button className="search-button">
-                                <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: "#ffffff", }} />
-                            </button>
-                        </div>
-                    </Col>
-                    <Col md={3}>
-                        <div className="head-service">
-                            <div className="head-service-logo">
-                                <a><FontAwesomeIcon icon={faHeadset} style={{ height: "40px", }} /></a>
-                            </div>
-                            <div className="head-service-content">
-                                <p>Hotline</p>
-                                <Link onClick={redirectCall} style={{ color: "black", }}>(+990) 123456789</Link>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col md={2}>
-                        <div className="head-shopping">
-                            <div className="head-shopping-wish">
-                                <Link><FontAwesomeIcon icon={faHeart} style={{ height: "25px", color: "black", }} /></Link>
-                            </div>
-                            <div className="head-shopping-cart">
-                                <Link><FontAwesomeIcon icon={faCartShopping} style={{ height: "25px", color: "black", }} /></Link>
-                                <div className="shopping-cart-counter">0</div>
-                            </div>
-                            <div className="head-shopping-menue">
-                                <Link><FontAwesomeIcon icon={faBars} style={{ height: "25px", color: "black", }} /></Link>
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-                <Row className='justify-content-end'>
+                <Row className='justify-content-end align-items-center'>
                     <Col md={9}>
                         <div className="nav-list">
                             <ul className='nav-pages'>
                                 <li className='nav-pages-single'><a>Home<FontAwesomeIcon icon={faChevronDown} style={{ padding: "0 0 0 10px", }} /></a>
-                                    <ul>
-                                        <li>test</li>
+                                    <ul className='nav-page-nested'>
+                                        <li className='nav-page-nested-single'>Shirts</li>
+                                        <li className='nav-page-nested-single'>T-Shirts</li>
+                                        <li className='nav-page-nested-single'>Trowsers</li>
                                     </ul>
                                 </li>
                                 <li className='nav-pages-single'><a>Shop<FontAwesomeIcon icon={faChevronDown} style={{ padding: "0 0 0 10px", }} /></a>
-
-                                    <ul>
-                                        <li>test</li>
+                                    <ul className='nav-page-nested'>
+                                        <li className='nav-page-nested-single'>Shirts</li>
+                                        <li className='nav-page-nested-single'>T-Shirts</li>
+                                        <li className='nav-page-nested-single'>Trowsers</li>
                                     </ul>
                                 </li>
-                                <li className='nav-pages-single'><a>Page<FontAwesomeIcon icon={faChevronDown} style={{ padding: "0 0 0 10px", }} /></a>
-                                    <ul>
-                                        <li>test</li>
+                                <li className='nav-pages-single'><a>Pages<FontAwesomeIcon icon={faChevronDown} style={{ padding: "0 0 0 10px", }} /></a>
+                                    <ul className='nav-page-nested'>
+                                        <li className='nav-page-nested-single'>Shirts</li>
+                                        <li className='nav-page-nested-single'>T-Shirts</li>
+                                        <li className='nav-page-nested-single'>Trowsers</li>
                                     </ul>
                                 </li>
                                 <li className='nav-pages-single'><a>Blog<FontAwesomeIcon icon={faChevronDown} style={{ padding: "0 0 0 10px", }} /></a>
-                                    <ul>
-                                        <li>test</li>
+                                    <ul className='nav-page-nested'>
+                                        <li className='nav-page-nested-single'>Shirts</li>
+                                        <li className='nav-page-nested-single'>T-Shirts</li>
+                                        <li className='nav-page-nested-single'>Trowsers</li>
                                     </ul>
                                 </li>
                                 <li className='nav-pages-single'><a>About</a>
+
                                 </li>
                                 <li className='nav-pages-single'><a>Contact</a>
                                 </li>
---
+++ src/Pages/Home/Navbar/Searchbar.js
@@ -0,0 +1,74 @@
+import { faBars, faCartShopping, faHeadset, faHeart, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../../Assets/img/Logo/logo.png';
+
+
+const Searchbar = () => {
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearch = (event) => {
+        setSearchQuery(event.target.value);
+    }
+    const redirectCall = (phoneNumber) => {
+        window.location.href = `tel:${phoneNumber}`;
+    };
+    return (
+        <>
+            <Container>
+                <Row className='align-items-center justify-content-between py-4'>
+                    <Col md={2}>
+                        <div className="head-logo">
+                            <img className='w-100' src={logo} alt="" />
+                        </div>
+                    </Col>
+                    <Col md={5}>
+                        <div className="head-search">
+                            <div className="search-container">
+                                <input className='search-input'
+                                    type="text"
+                                    placeholder="Search Here"
+                                    value={searchQuery}
+                                    onChange={handleSearch}
+                                />
+                            </div>
+                            <button className="search-button">
+                                <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: "#ffffff", }} />
+                            </button>
+                        </div>
+                    </Col>
+                    <Col md={3}>
+                        <div className="head-service">
+                            <div className="head-service-logo">
+                                <a><FontAwesomeIcon icon={faHeadset} style={{ height: "40px", }} /></a>
+                            </div>
+                            <div className="head-service-content">
+                                <p>Hotline</p>
+                                <Link onClick={redirectCall} style={{ color: "black", }}>(+990) 123456789</Link>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col md={2}>
+                        <div className="head-shopping">
+                            <div className="head-shopping-wish">
+                                <Link><FontAwesomeIcon icon={faHeart} style={{ height: "25px", color: "black", }} /></Link>
+                            </div>
+                            <div className="head-shopping-cart">
+                                <Link><FontAwesomeIcon icon={faCartShopping} style={{ height: "25px", color: "black", }} /></Link>
+                                <div className="shopping-cart-counter">0</div>
+                            </div>
+                            <div className="head-shopping-menue">
+                                <Link><FontAwesomeIcon icon={faBars} style={{ height: "25px", color: "black", }} /></Link>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+};
+
+export default Searchbar;
